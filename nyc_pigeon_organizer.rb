80dde0f7771469ff40a226b4cf322774e2b8b0fe
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-03 23:02:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -16,6 +16,21 @@ def nyc_pigeon_organizer(data)
       end
     end
   end
-  binding.pry
+  #List, pigKey = data, aryEle
+  #List, cglKey = data, cglKey
+  #List, cglVal = data, aryEleKey
+  dataList.each do |pigKey, pigValue|
+    pigValue.each do |listCglKey, listCglValue|
+      data.each do |cgl, cglVal|
+        cglVal.each do |cglKey, ary|
+          for i in 0...ary.length
+            if pigKey.to_s == ary[0]
+              dataList[pigKey][listCglKey] << cglKey.to_s
+            end
+          end
+        end
+      end
+    end
+  end
   return dataList
 end
