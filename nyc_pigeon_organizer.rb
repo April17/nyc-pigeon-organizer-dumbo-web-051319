47140f8247a7fa187a7be4256fbff4de2024ae7b
Fanqiang Meng <fanmeng0417@gmail.com>
2019-05-03 23:16:10 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -19,21 +19,18 @@ def nyc_pigeon_organizer(data)
   #List, pigKey = data, aryEle
   #List, cglKey = data, cglKey
   #List, cglVal = data, aryEleKey
-  dataList.each do |pigKey, pigValue|
-    pigValue.each do |listCglKey, listCglValue|
-      data.each do |cgl, cglVal|
-        if listCglKey.to_s == cgl.to_s
-          cglVal.each do |cglKey, ary|
-            for i in 0...ary.length
-              if pigKey.to_s == ary[0]
-                dataList[pigKey][listCglKey] << cglKey.to_s
-              end
+  data.each do |dataCgl, dataCglVal|
+    dataCglVal.each do |dataCglKey, dataPig|
+      for i in 0...dataPig.length
+        datalist.each do |listPigKey, listPigValue|
+          listPigValue.each do |listCglKey, listCglValue|
+            if listCglKey.to_s == dataCgl.to_s && listPigKey.to_s == dataPig[i]
+              datalist[listPigKey][listCglKey] << dataCglKey.to_s
             end
           end
         end
       end
     end
   end
-
   return dataList
 end
